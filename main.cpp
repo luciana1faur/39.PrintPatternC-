744e85ef93cc34b848bf5494bf0f7eb17479497f
luciana1faur <luciana_faur@yahoo.com>
2020-09-26 15:26:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << endl;
+  cout << " xxxx" << endl;
+  cout << "x    x       x        x" << endl;
+  cout << "x            x        x" << endl;
+  cout << "x         xxxxxxx  xxxxxxx" << endl;
+  cout << "x            x        x" << endl;
+  cout << "x    x       x        x" << endl;
+  cout << " xxxx" << endl;
+}
